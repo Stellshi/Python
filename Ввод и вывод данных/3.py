5d71fd76fc2ac6a72ef6f87edf2189f31530780c
Stellshi <simplepro47@gmail.com>
2022-01-09 01:18:14 +0300

    v2

--- Ввод и вывод данных/3.py
+++ Ввод и вывод данных/3.py
@@ -1,6 +1,15 @@
-a=input("Введите число 1 :") #Запрос у пользователя ввода 4х чисел
+'''a=input("Введите число 1 :") #Запрос у пользователя ввода 4х чисел
 b=input("Введите число 2 :")
 c=input("Введите число 3 :")
 d=input("Введите число 4 :")
 
-print("%.2f" % (int(a+b)/int(c+d)))
+print("%.2f" % (int(a+b)/int(c+d)))'''
+
+
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+n12 = n1 + n2
+n34 = n3 + n4
+print ("%.2f" % (n12/n34))
