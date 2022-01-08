fc07559fe83b859210280babce99935eca5b0a81
Stellshi <simplepro47@gmail.com>
2022-01-09 01:20:04 +0300

    v2

--- Ввод и вывод данных/2.py
+++ Ввод и вывод данных/2.py
@@ -1,7 +1,11 @@
-print ("Решите пример  4 * 100 - 54")
+'''print ("Решите пример  4 * 100 - 54")
 a =  input("Напишите ваш ответ: ")
 
 b = int(4 * 100 - 54)
 
 print("Правильный ответ:"+str(b))
+'''
 
+ex = input("4 * 100 - 54 = ")
+print ("Right answer is", 346)
+print ("Your answer is", ex)
