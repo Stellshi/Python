562383f4927d3068061923fbfa061d0f16770484
Stellshi <simplepro47@gmail.com>
2022-01-11 16:55:49 +0300

    upd

--- 8.Исключения и их обработка в Python/1.py
+++ 8.Исключения и их обработка в Python/1.py
@@ -5,9 +5,40 @@ try:
 except:
     print ("Что-то пошло не так")'''
     
-try:
+  #error  
+'''try:
     n=input('Введите целое число: ')
     n=int(n)
     print('Все нормально. Вы ввели число', n)
 except ValueError:
-    print("Вы ввели не целое число")
+    print("Вы ввели не целое число")'''
+   #3 errors 
+'''try:
+    a = float(input("Введите делимое: "))
+    b = float(input("Введите делитель: "))
+    c= a/b
+    print("Частное: %.2f" % c)
+except ValueError:
+    print("Нельзя вводить строки")
+except ZeroDivisionError:
+    print("Нельзя делить на ноль")
+    '''
+    #Группировка ошибок
+'''try:
+    a = float(input("Введите делимое: "))
+    b = float(input("Введите делитель: "))
+    c= a/b
+    print("Частное: %.2f" % c)
+except(ValueError, ZeroDivisionError):
+    print("Нельзя вводить строки или делить на ноль")'''
+    #Ошибки finally
+    
+try:
+    n= input('Vvedite 4islo: ')
+    n= int(n)
+except ValueError:
+    print("Neverno")
+else: 
+    print ('Vse OK, vashe shislo', n)
+finally:
+    print("end programs") 
