fc6994d184f1d4f3df3e89f018e58c89293749aa
Stellshi <simplepro47@gmail.com>
2022-01-21 01:34:02 +0300

    Симулятор ребенка

--- test/theree_year-old.py
+++ test/theree_year-old.py
@@ -7,8 +7,9 @@ from urllib import response
 print ("\tДобро пожаловать в прогрмму 'Симулятор трелетнего ребенка'\n")
 
 print("Имитируется разговор с маленьким ребенком")
-print("Попробуйте остановить этот кошмар \n")
-responce =""
-while response != "Потому что":
+print("Попробуйте остановить этот кошмар\n")
+responce ="1"
+while response != "1":
     responce = input("Почему?\n")
 print("А ладно")
+input("\n\nНажмите энтер чтобы выйти")
