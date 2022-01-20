635c4a505cbfa4a62fd74f4abc796bb926257aec
Stellshi <simplepro47@gmail.com>
2022-01-21 01:27:21 +0300

    New programs

--- test/4.py
+++ test/4.py
@@ -1,4 +1,8 @@
 # поиск символов с троке
 str1 = 'Привет! Как дела?'
 
-print(str1.find('!'))
+print(str1.replace("Привет","Вдруг"))
+
+print(str1.find('и')) #поиск по индексу 0 1 2 ...
+#print(str1.rfind('и!')) #поиск с правой строны
+print(str1.rindex('и'))
