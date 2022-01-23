22107a4844e20416d8bb38300464f94f18950afb
Stellshi <simplepro47@gmail.com>
2022-01-23 03:49:39 +0300

    ok

---
+++ 10.Циклы в программировании/1.py
@@ -0,0 +1,16 @@
+from tkinter import N
+
+
+n = input("Введите целоче число: ")
+
+while type(n) !=int:
+    try:
+        n=int(n)
+    except ValueError:
+        print("Неправильно ввели")
+        n=input("Введите целое число")
+        
+if n % 2 == 0:
+    print("Четное")
+else:
+    print("Нечетное")
