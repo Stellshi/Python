ecd1f69ceffceac7db90e86837fe2f56dd01cd68
Stellshi <simplepro47@gmail.com>
2022-01-26 04:50:23 +0300

    Create backup_ver1.py

---
+++ Копирование файлов/backup_ver1.py
@@ -0,0 +1,25 @@
+from distutils import command
+from itertools import count
+import os
+import time
+
+#1. Файлы и каталоги, которые необходимо скопировать, собираются в список.
+cource=['"C:\\Code"']
+
+#2. Резернвые копии должны храниться в основном каталоге резерва.
+
+target_dir = 'D:\\Copy' #Путь куда будет копироваться копия
+
+#3.Файлы помещаются в zip-архив
+#4.Именем для zip-архива служит текущая дата и время.
+
+target=target_dir +os.sep +time.strftime("%Y%m%d%H%M%S") + ".zip"
+
+#5.Используем команду zip  для помещения файлов в zip-архив
+zip_command = "zip -qr {0} {1}".format(target, ' '.join(cource))
+
+# Запускаем резервное копирование
+if os.system(zip_command) ==0:
+    print("Резервная копия успешно создана в ", target)
+else:
+    print("Создание резервной копии НЕ УДАЛОСЬ")
