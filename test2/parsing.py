5ea4fb66201101ad8769f5ad4ebfc70525282df6
Stellshi <simplepro47@gmail.com>
2022-02-13 15:38:39 +0300

    Update parsing.py

--- test2/parsing.py
+++ test2/parsing.py
@@ -6,7 +6,7 @@ from queue import Full
 
 spisok = []
 
-for adress, dirs, files  in os.walk('C:\Папка для примера'):
+for adress, dirs, files  in os.walk('C:\Папка для примера'): # 
     # spisok.append(adress)
     for file in files:
         full= os.path.join (adress, file)
