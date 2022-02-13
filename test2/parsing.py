010abf1e76abf9ea61d08b8de29790027b617d0b
Stellshi <simplepro47@gmail.com>
2022-02-13 15:37:24 +0300

    Update parsing.py

--- test2/parsing.py
+++ test2/parsing.py
@@ -1,6 +1,7 @@
 from ast import walk
 from importlib.metadata import files
 import os
+import time
 from queue import Full
 
 spisok = []
@@ -9,7 +10,8 @@ for adress, dirs, files  in os.walk('C:\Папка для примера'):
     # spisok.append(adress)
     for file in files:
         full= os.path.join (adress, file)
-        if '.txt' in full:
+        # if '.txt' in full:
+        if time.time() - os.path.getctime(full) < 86400:
             spisok.append(full) 
     
 print(spisok)
