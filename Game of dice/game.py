797418fd46d5d798b723aafbbb1bdc42be6ac35f
Stellshi <simplepro47@gmail.com>
2022-02-17 19:04:47 +0300

    Update game.py

--- Game of dice/game.py
+++ Game of dice/game.py
@@ -2,19 +2,36 @@ from cProfile import label
 from msilib.schema import File
 from tkinter import *
 from tkinter import font
+from tkinter.tix import IMAGE
+
+import random,time
+
+def bros(): 
+    x = random.choice(['b.png','b2.png','b3.png','b4.png','b5.png','b6.png'])
+    return x
+
+def img():
+    b1 = PhotoImage(file=(bros()))
+
+
 
 
 root = Tk()
 root.geometry('400x200')
 root.title('Игра в кости.')
 root.resizable(height=False, width=False)
-root.iconphoto(True,PhotoImage(file=('iconka.png')))
+root.iconphoto(True,PhotoImage(file=('C:\\Users\Stellshi\Documents\GitHub\Python\Game of dice\iconka.png')))
+font = PhotoImage(file=('C:\\Users\Stellshi\Documents\GitHub\Python\Game of dice\holst.png'))
+Label(root,image=font).pack()
+
+lab1 = Label(root)
+lab1.place(relx=0.3, rely=0.5, anchor=CENTER) #размещение метки по центру с координатами
+
+lab1 = Label(root)
+lab1.place(relx=0.7, rely=0.5, anchor=CENTER)
 
 
-font = PhotoImage(file=('holst.png'))
 
-
-# Label(root,image=font).pack()
-
+Button(root, text='Бросок', command=img).pack()
 
 root.mainloop()
