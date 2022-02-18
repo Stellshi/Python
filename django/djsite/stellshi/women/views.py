8d20cdd1fe9be9e4c9ad2659895e20fbde018693
Stellshi <simplepro47@gmail.com>
2022-02-19 01:38:42 +0300

    1

--- django/djsite/stellshi/women/views.py
+++ django/djsite/stellshi/women/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Страница приложения women.')\
+        
+def categories(request):
+    return HttpResponse('<h1>Статья по категориям</h1>')
